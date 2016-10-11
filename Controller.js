0ecba95ed20e669ef74c30dde965f6a6a8627a29
tamarav <baksuz89@hotmail.rs>
2016-10-11 14:54:58 +0200

    Update Controller.js

--- Controller.js
+++ Controller.js
@@ -1,6 +1,6 @@
 angular.module('mainApp', []).controller('Controller', function getTable($scope, $http){
-    $http.get("http://api.football-data.org/v1/competitions/433/leagueTable")
-    .then(function(response) {
-        $scope.myWelcome = response.data;
+    var url = "http://api.football-data.org/v1/competitions/433/leagueTable";
+	$http.get(url).success(function(response) {
+      //  $scope.answer = response.data;
     });
 });
